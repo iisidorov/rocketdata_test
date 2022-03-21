a735b788add0f7fb3b3f50c3d8edeb738247a144
Ivan Sidorov <iisidorov97@gmail.com>
2022-03-21 15:19:48 +0300

    Project skeleton Initial commit

---
+++ emplatform/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for emplatform project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emplatform.settings')
+
+application = get_asgi_application()
