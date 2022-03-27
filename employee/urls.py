f14c30085d2173ce69325f8b700746b6f14b652a
Ivan Sidorov <iisidorov97@gmail.com>
2022-03-28 01:42:07 +0300

    Add JWT Authorization + Profile view

--- employee/urls.py
+++ employee/urls.py
@@ -1,7 +1,20 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
+from rest_framework_simplejwt import views as jwt_views
 
 urlpatterns = [
-    path('employees/', views.EmployeeListView.as_view()),
-    path('employees/level/<int:level>/', views.EmployeeLevelListView.as_view({'get': 'list'})),
+    path('employees/',
+         views.EmployeeListView.as_view(),
+         name='employees'),
+    path('employees/level/<int:level>/',
+         views.EmployeeLevelListView.as_view({'get': 'list'}),
+         name='employees_level'),
+
+    path('token/', jwt_views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', jwt_views.TokenRefreshView.as_view(), name='token_refresh'),
+    path('register/', views.RegisterApi.as_view()),
+    path('token/blacklist/', views.BlacklistTokenView.as_view(), name='blacklist'),
+
+    path('profile/', views.UserProfileView.as_view({'get': 'retrieve'}), name='profile'),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- employee/views.py
+++ employee/views.py
@@ -1,21 +1,95 @@
 from django.shortcuts import render
-from rest_framework import permissions, generics, status, viewsets
+from django.http import JsonResponse
+from rest_framework import permissions, generics, status, viewsets, views
 from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework_simplejwt.tokens import RefreshToken
+from rest_framework.decorators import api_view
 from .permissions import IsApiAllowed
-from .serializers import EmployeeSerializer
-from .models import Employee
+from .serializers import EmployeeSerializer, UserSerializer
+from .models import Employee, User
 
 
-class EmployeeListView(generics.ListCreateAPIView):
+class EmployeeListView(generics.ListAPIView):
+    """
+    Url: employees/
+    Allowed requests: GET
+    """
     permission_classes = [IsAuthenticated & (IsAdminUser | IsApiAllowed)]
     serializer_class = EmployeeSerializer
     queryset = Employee.objects.all()
 
 
 class EmployeeLevelListView(viewsets.ModelViewSet):
+    """
+    Url: employees/level/<int:level>/
+    Allowed requests: GET
+    """
     permission_classes = [IsAuthenticated & (IsAdminUser | IsApiAllowed)]
     serializer_class = EmployeeSerializer
     queryset = Employee.objects.all()
 
     def get_queryset(self):
         return Employee.objects.filter(level=self.kwargs["level"])
+
+
+class UserProfileView(viewsets.ModelViewSet):
+    """
+    Url: profile/
+    Allowed requests: GET
+    * Returns Authenticated User's credentials
+    """
+    permission_classes = [IsAuthenticated & (IsAdminUser | IsApiAllowed)]
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def get_object(self):
+        queryset = self.filter_queryset(self.get_queryset())
+        obj = queryset.get(pk=self.request.user.id)
+        self.check_object_permissions(self.request, obj)
+        return obj
+
+
+class RegisterApi(generics.GenericAPIView):
+    """
+    Url: register/
+    Allowed requests: POST
+    JSON-body: {
+        "email": string,
+        "first_name": string,
+        "last_name": string,
+        "father_name": string,
+        "password": string
+    }
+    """
+    serializer_class = UserSerializer
+
+    def post(self, request, *args,  **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.save()
+        return JsonResponse({
+            "user": UserSerializer(user, context=self.get_serializer_context()).data,
+            "message": "User Created Successfully. Now perform Login to get your token",
+        }, status=status.HTTP_200_OK)
+
+
+# Save refresh-token to Blacklist after logout
+class BlacklistTokenView(views.APIView):
+    """
+    Url: logout/blacklist/
+    Allowed requests: POST
+    JSON-body: {"refresh": string}
+    * Blacklisting token after user's logout
+    """
+    permissions_classes = [permissions.AllowAny]
+
+    def post(self, request):
+        try:
+            refresh_token = request.data["refresh"]
+            token = RefreshToken(refresh_token)
+            token.blacklist()
+            return JsonResponse({"message": "Token Blacklisted Successfully"},
+                                status=status.HTTP_200_OK)
+        except Exception as e:
+            return JsonResponse({"message": "Error while Token Blacklisting"},
+                                status=status.HTTP_400_BAD_REQUEST)
